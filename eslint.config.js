22de5246443c4344d6b00250b01ac1ee06b0cfd9
w4.3nd0 <w4.3nd0@gmail.com>
2025-10-26 22:55:33 +0900

    chore: commit時にlintチェックとformat整形する様に修正 (#1)

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginPrettierRecommended from "eslint-config-prettier/recommended";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,4 +21,6 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  // NOTE: 他の構成を上書き出るように最後尾に記述
+  eslintPluginPrettierRecommended,
 ])
