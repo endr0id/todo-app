2a95a5eb88170f16d7bbfefbc8c0bb666e25d0af
w4.3nd0 <w4.3nd0@gmail.com>
2025-10-30 02:31:10 +0900

    feat: Install Storybook (#7)

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
