933e64bb7b1a3d1caeb5175aea5fab5a737c6019
w4.3nd0 <w4.3nd0@gmail.com>
2025-10-30 00:33:11 +0900

    chore: Lintルール追加 (#7)
    内容:
    - 未使用のモジュールを削除

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,27 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import importPlugin from "eslint-plugin-import";
+import unUsedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
   globalIgnores(["dist"]),
+  {
+    plugins: {
+      "unused-imports": unUsedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
