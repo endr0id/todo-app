38f254967704d7489d67f9ac29f41e8715b108b7
w4.3nd0 <w4.3nd0@gmail.com>
2025-11-11 00:35:32 +0900

    fix: Repository修正 (#15)
    内容:
    - 必要最小限の記述
    - Entity変換は拡張関数化して変換

--- backend/todo/src/main/kotlin/com/example/todo/features/backlog/data/repository/BacklogRepositoryImpl.kt
+++ backend/todo/src/main/kotlin/com/example/todo/features/backlog/data/repository/BacklogRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.example.todo.features.backlog.data.model.ColumnEntity
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 import org.jooq.generated.tables.Columns
+import org.jooq.generated.tables.records.ColumnsRecord
 
 @Repository
 class BacklogRepositoryImpl(
@@ -12,21 +13,17 @@ class BacklogRepositoryImpl(
     override fun findAllColumns(): List<ColumnEntity> {
         val columns = Columns.COLUMNS
 
-        return dsl.select(
-            columns.ID,
-            columns.TITLE,
-            columns.DESCRIPTION,
-            columns.POSITION
-        )
-            .from(columns)
+        return dsl.selectFrom(columns)
             .orderBy(columns.POSITION.asc())
-            .fetch { record ->
-                ColumnEntity(
-                    id = record.get(columns.ID) ?: 0L,
-                    title = record.get(columns.TITLE),
-                    description = record.get(columns.DESCRIPTION),
-                    position = record.get(columns.POSITION) ?: 0
-                )
-            }
+            .fetch { it.toEntity() }
+    }
+
+    private fun ColumnsRecord.toEntity(): ColumnEntity {
+        return ColumnEntity(
+            id = this.id,
+            title = this.title,
+            description = this.description,
+            position = this.position
+        )
     }
 }
