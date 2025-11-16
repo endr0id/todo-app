f666508c41ec1453ccdbcdc2a2dc4a3b821edf70
w4.3nd0 <w4.3nd0@gmail.com>
2025-11-17 00:26:47 +0900

    feat: backlog/のレイアウト調整 (#19)

--- frontend/src/layouts/backlog/BacklogLayout.tsx
+++ frontend/src/layouts/backlog/BacklogLayout.tsx
@@ -1,14 +1,22 @@
 import { css } from "../../../styled-system/css";
 
+const backlogWrapperRecipe = css({
+  display: "flex",
+  direction: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+});
+
+const cardRecipe = css({
+  display: "flex",
+  gap: "5px",
+});
+
 const BacklogLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div
-      className={css({
-        display: "flex",
-        gap: "5px",
-      })}
-    >
-      {children}
+    <div className={backlogWrapperRecipe}>
+      <div className={cardRecipe}>{children}</div>
     </div>
   );
 };
