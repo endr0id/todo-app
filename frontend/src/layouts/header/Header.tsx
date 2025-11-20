ba7ab28fde11df64c24d56381c8cf2c48db98751
w4.3nd0 <w4.3nd0@gmail.com>
2025-11-21 02:01:46 +0900

    chore: 使用していないコンポーネント削除 (#19)

--- frontend/src/layouts/header/Header.tsx
+++
@@ -1,5 +0,0 @@
-const Header = ({ children }: { children: React.ReactNode }) => {
-  return <header>{children}</header>;
-};
-
-export default Header;
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,16 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import ToolBar from "../components/toolbar/ToolBar";
-import Header from "../layouts/header/Header";
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => (
   <>
     <QueryClientProvider client={queryClient}>
-      <Header>
-        <ToolBar />
-      </Header>
       <Outlet />
     </QueryClientProvider>
   </>
